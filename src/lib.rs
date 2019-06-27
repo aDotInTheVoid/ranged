5d4e23b6b58c6b2c36bd8c0864e25c2ad92ce57c
Nixon <nixon.emoony@gmail.com>
2019-06-27 16:33:12 +0100

    basics

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+
+pub trait Num: Copy + num_traits::NumAssign{}
+
+#[derive(Clone, Copy)]
+pub struct Range<T: Num>{
+    min: T,
+    max: T,
+}
+
+impl<T: Num> Num for Range<T>{
+
+}
+
+
+
 #[cfg(test)]
 mod tests {
     #[test]
